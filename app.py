52823b81d2aa012385da7a8bdbe3d94d7a421e30
antilotus <109603408+antilotus@users.noreply.github.com>
2025-12-01 11:14:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,8 @@ with col2:
     display_tile("disp_frame_10.png", "pages/NPAchargesTMT.py", " ADLD")
 
 with col3:
-    display_tile("charges_frame_10.png", "pages/NPAchargesTMT.py", " Mulliken Charges")
-
-# Path to your local PDB file
-pdb_path = "./Media/SPE2.pdb"
+    display_tile("charges_frame_10.png", "pages/AgOR28vis.py", " Mulliken Charges")
+    
 # ---- Contact Section ----
 st.header("📬 Contact")
 
@@ -39,84 +37,6 @@ with st.form("contact_form"):
 
     submitted = st.form_submit_button("Send Email")
 
-    # Show full protein in cartoon
-    view.setStyle({'cartoon': {'color': 'white'}})
-
-    # Optional: zoom to whole protein first (you can adjust this)
-    view.zoomTo()
-
-    # Zoom to specific resnames (only if they exist)
-    view.setStyle({'resn': 'LIG'}, {'stick': {'colorscheme': 'GreyCarbon'}})
-    #view.setStyle({'resn': 'NPH'}, {'stick': {'colorscheme': 'GreenCarbon'}})
-
-    view.addLabel("TMT", {
-        'position': {'resn': "LIG"},
-        'backgroundColor': 'blue',
-        'fontColor': 'black',
-        'fontSize': 12
-    })
-
-    #view.addLabel("NPH", {
-    #    'position': {'resn': 'NPH'},
-    #    'backgroundColor': 'blue',
-    #    'fontColor': 'blue',
-    #    'fontSize': 12
-    #})
-    # view.zoomTo({'resn': ['DHK', 'NPH']})
-
-    # Licorice for specific residue numbers
-    highlight_residues = [1466,1490,1493,1494,1497,1562,1589,1614,1617,1618,1621,1725]
-    for resid in highlight_residues:
-        view.setStyle({'resi': str(resid)}, {'stick': {'colorscheme': 'RedCarbon'}})
-        view.addLabel(f"Resid {resid}", {
-            'position': {'resi': str(resid)},
-            'backgroundColor': 'white',
-            'fontColor': 'red',
-            'fontSize': 10
-        })
-
-
-
-
-
-    # Set orientation manually (rotation in degrees)
-    view.rotate(0, 'x')
-    view.rotate(0, 'y')
-    view.rotate(0, 'z') 
-    view.zoomTo({'or': [
-        {'resn': 'LIG'},
-        {'resi': [str(r) for r in highlight_residues]}
-    ]})
-
-    view.setBackgroundColor('white')
-    st.components.v1.html(view._make_html(), height=500)
-
-
-else:
-    st.error(f"PDB file not found at: {pdb_path}")
-
-
-
-
-
-# --- Section 5: Video ---
-#st.header("Video")
-#st.video("../media.mp4",autoplay=True)  # Supports local or URL path
-
-# --- Section 6: Markdown and Uploads ---
-# st.header("Upload & Notes")
-# uploaded_file = st.file_uploader("Upload a CSV file")
-# if uploaded_file:
-#     df_uploaded = pd.read_csv(uploaded_file)
-#     st.write("Uploaded Data Preview:")
-#     st.dataframe(df_uploaded)
-
-# st.markdown("""
-# ### Notes
-# - You can use this portal to showcase results dynamically.
-# - Combine static images with interactive elements.
-# - Perfect for defending or sharing complex bioinformatics visualizations.
-# """)
     if submitted:
         mailto_link = f"mailto:Albert.THORHALLSSON@univ-cotedazur.fr?subject={subject}&body=From: {name} ({email})%0A%0A{message}"
         st.markdown(f"[📨 Click here to send via your email client]({mailto_link})", unsafe_allow_html=True)
