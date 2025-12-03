7e2cd98debf5ed3b79ebfed8f1762e80d810135c
Albert Thor <antilotus@hotmail.com>
2025-12-03 10:25:55 +0100

    Multipage test

---
+++ Pages/NPAallQM.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import py3Dmol
+import os
+
+st.set_page_config(page_title="ChargesallQM", layout="wide")
+
+st.title("All QM region heatmaps")
+
+st.header('Natural Population Analysis Charges all QM atoms')
+video_file = open("../Media/NPAfullqm.mp4", "rb")
+video_bytes = video_file.read()
+st.video(video_bytes)
+st.image('../Media/NBOcharges_colorbar.png', width=1200)
+st.header('Mulliken Charges all QM atoms')
+video_file1 = open("../Media/Mullikenfullqm.mp4", "rb")
+video_bytes1 = video_file1.read()
+st.video(video_bytes1)
+st.image('../Media/mulliken_colorbar.png', width=1200)
+st.page_link("app.py", label="Home")
--- app.py
+++ app.py
@@ -2,29 +2,31 @@ import streamlit as st
 import py3Dmol
 import os
 
-st.set_page_config(page_title="SFCi poster ESI", layout="wide")
+st.set_page_config(page_title="Home", layout="wide")
 
-st.title("🎓 Welcome to the SFCi poster ESI")
+st.title("🎓 Welcome to the my SFCi poster ESI")
+st.header('By Albert Thor Thorhallson')
 
 # Uniform image display size (e.g., 250px height)
-st.header('Natural Population Analysis Charges')
+st.subheader('Natural Population Analysis Charges')
 st.image('Media/NBO.gif')
 st.image('Media/NBOcharges_colorbar.png', width=800)
-st.header('Atomic Decomposition of London Dispersion')
+st.subheader('Atomic Decomposition of London Dispersion')
 st.image('Media/dispersion.gif', width=500)
 st.image('Media/disp_colorbar.png', width=500)
-st.header('Mulliken Analysis Charges')
+st.subheader('Mulliken Analysis Charges')
 st.image('Media/mullikencharges.gif')
 st.image('Media/mulliken_colorbar.png', width=800)
-st.header('Natural Population Analysis Charges all QM atoms')
-video_file = open("Media/NPAfullqm.mp4", "rb")
-video_bytes = video_file.read()
-st.video(video_bytes)
-st.image('Media/NBOcharges_colorbar.png', width=1200)
-st.header('Mulliken Charges all QM atoms')
-video_file1 = open("Media/Mullikenfullqm.mp4", "rb")
-video_bytes1 = video_file1.read()
-st.video(video_bytes1)
+st.page_link("Pages/NPAallQM.py", label="ChargesallQM")
+#st.header('Natural Population Analysis Charges all QM atoms')
+#video_file = open("Media/NPAfullqm.mp4", "rb")
+#video_bytes = video_file.read()
+#st.video(video_bytes)
+#st.image('Media/NBOcharges_colorbar.png', width=1200)
+#st.header('Mulliken Charges all QM atoms')
+#video_file1 = open("Media/Mullikenfullqm.mp4", "rb")
+#video_bytes1 = video_file1.read()
+#st.video(video_bytes1)
 st.image('Media/mulliken_colorbar.png', width=1200)
 st.header('TMT escaping the binding site')
 video_file2 = open("Media/TMTescapes.mp4", "rb")
@@ -45,73 +47,74 @@ Quantum Mechanics/Molecular Mechanics (QM/MM) single point energy (SPE) calculat
 <h1>''',
 unsafe_allow_html=True)
 
-# Path to your local PDB file
-pdb_path = "./Media/SPE2.pdb"
-
-if os.path.exists(pdb_path):
-    with open(pdb_path, "r") as f:
-        pdb_data = f.read()
-
-    # Load and visualize the PDB
-    view = py3Dmol.view(width=800, height=500)
-    view.addModel(pdb_data, "pdb")
-
-    # Show full protein in cartoon
-    view.setStyle({'cartoon': {'color': 'white'}})
-
-    # Optional: zoom to whole protein first (you can adjust this)
-    view.zoomTo()
-
-    # Zoom to specific resnames (only if they exist)
-    view.setStyle({'resn': 'LIG'}, {'stick': {'colorscheme': 'GreyCarbon'}})
-    #view.setStyle({'resn': 'NPH'}, {'stick': {'colorscheme': 'GreenCarbon'}})
-
-    view.addLabel("TMT", {
-        'position': {'resn': "LIG"},
-        'backgroundColor': 'blue',
-        'fontColor': 'black',
-        'fontSize': 12
-    })
-
-    #view.addLabel("NPH", {
-    #    'position': {'resn': 'NPH'},
-    #    'backgroundColor': 'blue',
-    #    'fontColor': 'blue',
-    #    'fontSize': 12
-    #})
-    # view.zoomTo({'resn': ['DHK', 'NPH']})
-
-    # Licorice for specific residue numbers
-    highlight_residues = [1466,1490,1493,1494,1497,1562,1589,1614,1617,1618,1621,1725]
-    for resid in highlight_residues:
-        view.setStyle({'resi': str(resid)}, {'stick': {'colorscheme': 'RedCarbon'}})
-        view.addLabel(f"Resid {resid}", {
-            'position': {'resi': str(resid)},
-            'backgroundColor': 'white',
-            'fontColor': 'red',
-            'fontSize': 10
-        })
-
-
-
-
-
-    # Set orientation manually (rotation in degrees)
-    view.rotate(0, 'x')
-    view.rotate(0, 'y')
-    view.rotate(0, 'z') 
-    view.zoomTo({'or': [
-        {'resn': 'LIG'},
-        {'resi': [str(r) for r in highlight_residues]}
-    ]})
-
-    view.setBackgroundColor('white')
-    st.components.v1.html(view._make_html(), height=500)
-
-
-else:
-    st.error(f"PDB file not found at: {pdb_path}")
-
+st.page_link("Pages/AgOR28vis.py", label="AgVis")
+## Path to your local PDB file
+#pdb_path = "./Media/SPE2.pdb"
+#
+#if os.path.exists(pdb_path):
+#    with open(pdb_path, "r") as f:
+#        pdb_data = f.read()
+#
+#    # Load and visualize the PDB
+#    view = py3Dmol.view(width=800, height=500)
+#    view.addModel(pdb_data, "pdb")
+#
+#    # Show full protein in cartoon
+#    view.setStyle({'cartoon': {'color': 'white'}})
+#
+#    # Optional: zoom to whole protein first (you can adjust this)
+#    view.zoomTo()
+#
+#    # Zoom to specific resnames (only if they exist)
+#    view.setStyle({'resn': 'LIG'}, {'stick': {'colorscheme': 'GreyCarbon'}})
+#    #view.setStyle({'resn': 'NPH'}, {'stick': {'colorscheme': 'GreenCarbon'}})
+#
+#    view.addLabel("TMT", {
+#        'position': {'resn': "LIG"},
+#        'backgroundColor': 'blue',
+#        'fontColor': 'black',
+#        'fontSize': 12
+#    })
+#
+#    #view.addLabel("NPH", {
+#    #    'position': {'resn': 'NPH'},
+#    #    'backgroundColor': 'blue',
+#    #    'fontColor': 'blue',
+#    #    'fontSize': 12
+#    #})
+#    # view.zoomTo({'resn': ['DHK', 'NPH']})
+#
+#    # Licorice for specific residue numbers
+#    highlight_residues = [1466,1490,1493,1494,1497,1562,1589,1614,1617,1618,1621,1725]
+#    for resid in highlight_residues:
+#        view.setStyle({'resi': str(resid)}, {'stick': {'colorscheme': 'RedCarbon'}})
+#        view.addLabel(f"Resid {resid}", {
+#            'position': {'resi': str(resid)},
+#            'backgroundColor': 'white',
+#            'fontColor': 'red',
+#            'fontSize': 10
+#        })
+#
+#
+#
+#
+#
+#    # Set orientation manually (rotation in degrees)
+#    view.rotate(0, 'x')
+#    view.rotate(0, 'y')
+#    view.rotate(0, 'z') 
+#    view.zoomTo({'or': [
+#        {'resn': 'LIG'},
+#        {'resi': [str(r) for r in highlight_residues]}
+#    ]})
+#
+#    view.setBackgroundColor('white')
+#    st.components.v1.html(view._make_html(), height=500)
+#
+#
+#else:
+#    st.error(f"PDB file not found at: {pdb_path}")
+#
 
 st.header('References')
 st.write('(1) WHO. 2024. Global strategic preparedness, readiness and response plan for dengue and other Aedes-borne arbovirus')
